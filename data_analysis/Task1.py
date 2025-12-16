8422ab5f9dd19af375d2dcac122f5c2aed2416d9
Maria-kvetsko <m.e.r.r.y.k.v.z@gmail.com>
2025-12-16 19:21:17 +0200

    update task
    Змінила датасет з яким працювала, та змінила назви колонок в дослідженні для правильного відображення даних

---
+++ data_analysis/Task1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('adult_data.csv')
+print(data.info())
+print(data['age'].unique())
+# 1. Скільки чоловіків і жінок ( статева ознака) представлено в цьому наборі даних?
+print(data['sex'].value_counts())
+''' Sex
+Male      10860 
+Female     5421'''
