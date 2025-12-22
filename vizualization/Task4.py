6019e26d1f81ad268d559ebf1fffcfa3b7477ccc
Maria-kvetsko <m.e.r.r.y.k.v.z@gmail.com>
2025-12-22 12:09:17 +0200

    Update task 4
    Виправила помилку в обчиленні умов задачі

--- vizualization/Task4.py
+++ vizualization/Task4.py
@@ -2,6 +2,6 @@
 import pandas as pd
 df = pd.read_csv('vizualization/mlbootcamp5_train.csv')
 print(df.info())
-df['age'] = df['age']/12
+df['age'] = df['age']/365.25
 mean_age = df.groupby(['smoke'])['age'].median()
-print(round(mean_age[0]-mean_age[1]))
+print(round((mean_age[0]*12)-(mean_age[1]*12)))
