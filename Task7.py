38ce4e22c4c82d271672f524acd3c45f5c218fca
Maria-kvetsko <m.e.r.r.y.k.v.z@gmail.com>
2026-01-05 16:46:05 +0200

    done task7

---
+++ Task7.py
@@ -0,0 +1,20 @@
+'''
+Яка пара ознак має найсильнішу кореляцію Пірсона з ознакою статі ?
+
+Кардіо, Холестерин
+
+Висота, Куріння
+
+Куріння, Алкоголь
+
+Зріст, Вага
+'''
+import pandas as pd
+df = pd.read_csv('vizualization/mlbootcamp5_train.csv')
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+corr_matrix = df.corr()
+plt.figsize
+sns.heatmap(corr_matrix, annot=True)
+plt.show()
