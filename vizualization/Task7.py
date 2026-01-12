6f82d915054bc80195c4185381fb4e5183e80eac
Maria-kvetsko <m.e.r.r.y.k.v.z@gmail.com>
2026-01-12 12:15:43 +0200

    update task7

--- vizualization/Task7.py
+++ vizualization/Task7.py
@@ -14,7 +14,7 @@ df = pd.read_csv('vizualization/mlbootcamp5_train.csv')
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-corr_matrix = df.corr()
-plt.figsize
+corr_matrix = df.corr(method='pearson')
+
 sns.heatmap(corr_matrix, annot=True)
 plt.show()
